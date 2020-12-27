f334b2302f2f0f6a2dc570eab70471b44c34977b
dongpo <dongbianshanpo@163.com>
2020-12-27 20:14:03 +0800

    增加 “第3章 使用MySQL.sql” 笔记

---
+++ 第3章 使用MySQL.sql
@@ -0,0 +1,18 @@
+# 第3章 使用MySQL
+
+# 选择数据库
+USE mysql_bizhibihui;
+
+# 了解数据库和表
+SHOW DATABASES;
+SHOW TABLES;
+SHOW COLUMNS FROM customers;
+DESCRIBE customers;
+
+# 其他
+SHOW STATUS;
+SHOW CREATE DATABASE mysql_bizhibihui;
+SHOW CREATE TABLE customers;
+SHOW GRANTS;
+SHOW ERRORS;
+SHOW WARNINGS;
